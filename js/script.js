32783bcc07793e46710da06b0640728b0a6eb218
riyanslr10 <riyan.putras40@gmail.com>
2021-12-20 01:25:26 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// sidebar
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide');
+});
+
+// owl carousel script
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 0,
+    autoplaySpeed: 6000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        }
+    }
+});
